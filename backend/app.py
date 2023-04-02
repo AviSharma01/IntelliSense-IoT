4807a26b60d66507d506f5b0e128c91ebc81b665
AviSharma01 <avisharma1204@gmail.com>
2023-04-02 02:52:24 -0700

    Updated .env file and app.py

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,65 @@
+import boto3
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
+# Initialize Flask app
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
+# Initialize SQLAlchemy database object
 db = SQLAlchemy(app)
+
+# Initialize JWT manager object
 jwt = JWTManager(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Initialize AWS IoT client object
+iot = boto3.client('iot')
 
+# Define function to handle incoming data
+def iot_data_handler(payload):
+    # Extract the desired data from the payload
+    data = json.loads(payload)
+    temperature = data['temperature']
+    humidity = data['humidity']
+
+    # Insert data into the database
+    new_data = TemperatureData(temperature=temperature, humidity=humidity)
+    db.session.add(new_data)
+    db.session.commit()
+
+    # Update the shadow
+    shadow.update_thing_shadow(
+        thingName='my_thing',
+        payload=json.dumps({
+            'state': {
+                'reported': {
+                    'temperature': temperature,
+                    'humidity': humidity
+                }
+            }
+        })
+    )
+
+
+# Subscribe to the IoT topic
+iot.subscribe(
+    topic='my_topic',
+    qos=1,
+    callback=iot_data_handler
+)
+
+# Define route to get temperature data
 @app.route('/api/temperature')
 def get_temperature():
-    # Get temperature data from database
-    temperature_data = Temperature.query.all()
-
-    # Convert database objects to dictionary format
-    temperature_list = []
-    for temperature in temperature_data:
-        temperature_dict = {
-            'timestamp': temperature.timestamp,
-            'temperature': temperature.temperature
-        }
-        temperature_list.append(temperature_dict)
-
-    # Return temperature data as JSON
-    return jsonify(temperature_list)
+    # TODO: Implement logic to get temperature data from the database
+    temperature_data = [{'timestamp': '2022-01-01T00:00:00Z', 'temperature': 20},
+                        {'timestamp': '2022-01-01T01:00:00Z', 'temperature': 21},
+                        {'timestamp': '2022-01-01T02:00:00Z', 'temperature': 22},
+                        {'timestamp': '2022-01-01T03:00:00Z', 'temperature': 23},
+                        {'timestamp': '2022-01-01T04:00:00Z', 'temperature': 24}]
+    return jsonify(temperature_data)
 
 if __name__ == '__main__':
     app.run()
+
