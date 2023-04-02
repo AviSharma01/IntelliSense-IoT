951587e56366e62b922f470e2feb2da036406a96
AviSharma01 <avisharma1204@gmail.com>
2023-04-02 02:54:30 -0700

    Added AWS related settings to settings file

--- backend/settings.py
+++ backend/settings.py
@@ -18,8 +18,17 @@ class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URI: str
     SQLALCHEMY_POOL_RECYCLE: int = 900
 
+    # AWS related settings
+    AWS_ACCESS_KEY_ID: str = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY: str = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_DEFAULT_REGION: str = os.getenv('AWS_DEFAULT_REGION')
+    AWS_IOT_ENDPOINT: str = os.getenv('AWS_IOT_ENDPOINT')
+    AWS_KINESIS_STREAM_NAME: str = os.getenv('AWS_KINESIS_STREAM_NAME')
+    AWS_IOT_THING_NAME: str = os.getenv('AWS_IOT_THING_NAME')
+
     class Config:
         env_file = ".env"
+
 
 
 settings = Settings()
